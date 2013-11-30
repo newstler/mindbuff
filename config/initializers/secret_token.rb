5bcb9207c211e8aafc9179db7725b8cf8f164e2a
Yuri Sidorov <cpu@robotinthecloak.com>
2013-11-30 15:24:40 +0400

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MindBuff::Application.config.secret_key_base = '8b23aa751345fd14d438fff3f0f6440b5b944cd676e01559141ad67318a47eaba12d81bbe28f1dfd4058211efe57d95b06f117c6f80c8903b2879698937697ef'
